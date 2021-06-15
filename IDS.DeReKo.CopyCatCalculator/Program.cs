e9c9216787d052673ce5b9802799f8a60565581f
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2021-06-15 10:38:11 +0200

    // commit

--- IDS.DeReKo.CopyCatCalculator/Program.cs
+++ IDS.DeReKo.CopyCatCalculator/Program.cs
@@ -14,49 +14,71 @@ namespace IDS.DeReKo.CopyCatCalculator
     {
       var baseDir = args[0];
       var minimum = int.Parse(args[1]);
-      var searchFiles = Directory.GetFiles(baseDir, args[2]);
-
-      var searchQueries = searchFiles.Select(ReadCorpusExplorerOutput)
-                                     .SelectMany(tmp => tmp)
-                                     .ToDictionary(x => x.Key, x => x.Value);
-
       var allFiles = Directory.GetFiles(baseDir, "*.tsv");
-      var filter = new HashSet<string>(searchFiles);
 
-      var @lock = new object();
-      using (var fs = new FileStream(args[3], FileMode.Create, FileAccess.Write))
+      var lock1 = new object();
+      var model = new Dictionary<string, Dictionary<Guid, SpamSumSignature>>();
+      Console.Write("Load Model...");
+      Parallel.ForEach(allFiles, file =>
+      {
+        var entry = ReadCorpusExplorerModelEntry(file);
+        lock (lock1)
+          model.Add(Path.GetFileName(file), entry);
+      });
+      Console.WriteLine("ok!");
+
+      var filter = new HashSet<string>(Directory.GetFiles(baseDir, args[2]).Select(Path.GetFileName));
+
+      var lock2 = new object();
+      var cnt = 0;
+      var max = allFiles.Length * filter.Count;
+
+      using (var fs = new FileStream(args[3], FileMode.Create, FileAccess.Write, FileShare.Read))
       using (var bf = new BufferedStream(fs, 4096))
-      using (var writer = new StreamWriter(bf))
-        Parallel.ForEach(allFiles, file =>
-        {
-          if (filter.Contains(file))
-            return;
-
-          Console.WriteLine($">>> {file}");
-
-          var content = ReadCorpusExplorerOutput(file);
-          foreach (var c in content)
+      using (var writer = new StreamWriter(bf, Encoding.UTF8))
+        Parallel.ForEach(filter, new ParallelOptions { MaxDegreeOfParallelism = 2 }, searchFile =>
           {
-            foreach (var s in searchQueries)
+            var searchQueries = model[searchFile];
+
+            Parallel.ForEach(allFiles, new ParallelOptions { MaxDegreeOfParallelism = 4 }, file =>
             {
-              var size = FuzzyHashing.Compare(c.Value, s.Value);
-              if (size < minimum)
-                continue;
+              lock (lock2)
+                cnt++;
 
-              lock (@lock)
-                writer.WriteLine(string.Join("\t", 
-                                             s.Key.ToString("N"), 
-                                             size.ToString(),
-                                             file, 
-                                             c.Key));
-            }
-          }
+              file = Path.GetFileName(file);
+              if (filter.Contains(file))
+                return;
 
-          Console.WriteLine($"<<< {file}");
-        });
+              Console.WriteLine($">>> {file}");
+
+              var content = model[file];
+              var cache = new List<string>();
+              var lock3 = new object();
+
+              Parallel.ForEach(searchQueries, new ParallelOptions { MaxDegreeOfParallelism = 10 }, s =>
+              {
+                foreach (var c in content)
+                {
+                  var size = FuzzyHashing.Compare(c.Value, s.Value);
+                  if (size < minimum)
+                    continue;
+
+                  lock (lock3)
+                    cache.Add(string.Join("\t", Path.GetFileName(searchFile), s.Key.ToString("N"), size.ToString(), Path.GetFileName(file), c.Key.ToString("N")));
+                }
+              });
+
+              lock (lock1)
+                foreach (var line in cache)
+                  writer.WriteLine(line);
+
+              lock (lock2)
+                Console.WriteLine($"<<< [{cnt} / {max}] {file} (CopyCats: {cache.Count})");
+            });
+          });
     }
 
-    private static Dictionary<Guid, SpamSumSignature> ReadCorpusExplorerOutput(string path)
+    private static Dictionary<Guid, SpamSumSignature> ReadCorpusExplorerModelEntry(string path)
     {
       var res = new Dictionary<Guid, SpamSumSignature>();
 
